cf9adda713e1914affdb063d99c6ca106ac5cf6b
Erick Chandra <erickchandra.1@gmail.com>
2017-12-16 13:16:34 +0700

    Detach unnecessary server environment.

--- team9-blockchain-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/var/www/grouptwo/group2-blockchain-anaviz/team9-blockchain-env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
