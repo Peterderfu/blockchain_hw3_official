26e2f18a3574fb3c8358a58f77db48f52fe10ba7
Erick Chandra <erickchandra.1@gmail.com>
2017-12-19 16:37:58 +0700

    Added tabs, modified links, and renaming files.

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     url(r'^$', views.landing, name='index'),
-    url(r'^price-realtime', views.priceRealtime, name="priceRealtime"),
+    url(r'^dashboard', views.dashboard, name="dashboard"),
     url(r'^index', views.indexSipun, name="indexSipun"),
     url(r'^index.html', views.indexSipun, name="indexSipun"),
     url(r'^signup', views.signupSipun, name="signupSipun"),
