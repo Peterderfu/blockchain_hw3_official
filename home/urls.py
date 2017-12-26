3be5cce66214471b3078b63a6f0ee8dcfbd3e877
Hao-Ying Liang <hyl0327@gmail.com>
2017-12-26 22:19:41 +0800

    More specified home.urls

--- home/urls.py
+++ home/urls.py
@@ -5,10 +5,9 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     url(r'^$', views.landing, name='index'),
-    url(r'^dashboard', views.dashboard, name="dashboard"),
-    url(r'^index', views.indexSipun, name="indexSipun"),
-    url(r'^index.html', views.indexSipun, name="indexSipun"),
-    url(r'^signup', views.signupSipun, name="signupSipun"),
-    url(r'^orderForm', views.orderFormSipun, name="orderFormSipun"),
-    url(r'^register', views.registerSipun, name="registerSipun"),
+    url(r'^dashboard/$', views.dashboard, name="dashboard"),
+    url(r'^index/$', views.indexSipun, name="indexSipun"),
+    url(r'^signup/$', views.signupSipun, name="signupSipun"),
+    url(r'^orderForm/$', views.orderFormSipun, name="orderFormSipun"),
+    url(r'^register/$', views.registerSipun, name="registerSipun"),
 ]
